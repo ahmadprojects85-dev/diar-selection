b25aa148d6e1a351a8a2c040755e424272a1dd5c
ahmadprojects85-dev <ahmadprojects85@gmail.com>
2026-07-23 12:11:34 +0300

    Use white logo in navbar

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import Image from 'next/image';
 
 interface LogoProps {
   className?: string;
   width?: number;
   height?: number;
   useFullLogo?: boolean;
+  isNav?: boolean;
 }
 
-export function Logo({ className = "", width = 200, height = 200, useFullLogo = true }: LogoProps) {
-  const imageSrc = "/brand-logo.png";
+export function Logo({ className = "", width = 200, height = 200, useFullLogo = true, isNav = false }: LogoProps) {
+  const imageSrc = isNav ? "/logo-white.png?v=2" : "/brand-logo.png?v=3";
 
   return (
-    <Image 
+    <img 
       src={imageSrc} 
       alt="Diar Selection Logo" 
       width={width} 
       height={height}
       className={`object-contain ${className}`}
-      priority
     />
   );
 }
