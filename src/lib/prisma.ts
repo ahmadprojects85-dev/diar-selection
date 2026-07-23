66a12bab736bc69571d69e15c42e8f1c13f564da
ahmadprojects85-dev <ahmadprojects85@gmail.com>
2026-07-23 11:42:13 +0300

    fix cloudflare build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client/wasm";
 import { connect } from "@tidbcloud/serverless";
 import { PrismaTiDBCloud } from "@tidbcloud/prisma-adapter";
 
